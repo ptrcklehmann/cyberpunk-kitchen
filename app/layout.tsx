130fd1529ece3e3ad8164f32d57b14181b97bbc8
Patrick Lehmann <ptrcklehmann.com>
2025-07-02 11:50:17 +0200

    refactor: remove unused CSS variables and dark mode styles from globals.css + cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "./globals.css";
 import clsx from "clsx";
 import { Metadata, Viewport } from "next";
 
@@ -37,7 +37,7 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen text-foreground bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
