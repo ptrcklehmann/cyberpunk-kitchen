51965957be98deaac79a63c2ed264c33b1c03501
Patrick Lehmann <ptrcklehmann.com>
2025-07-04 17:08:59 +0200

    chore(format): standardize code formatting and improve comment clarity

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'www.datocms-assets.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.datocms-assets.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
